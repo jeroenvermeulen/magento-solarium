4615b85ecde224b99e6e7178f5f60ddfbc9b365d
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2014-07-28 22:58:56 +0200

    Override to remove locking of search tables during reindex.

---
+++ app/code/community/JeroenVermeulen/Solarium/Model/CatalogSearch/Indexer/Fulltext.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * JeroenVermeulen_Solarium
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category    JeroenVermeulen
+ * @package     JeroenVermeulen_Solarium
+ * @copyright   Copyright (c) 2014 Jeroen Vermeulen (http://www.jeroenvermeulen.eu)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class JeroenVermeulen_Solarium_Model_CatalogSearch_Indexer_Fulltext extends Mage_CatalogSearch_Model_Indexer_Fulltext {
+
+    /**
+     * Override to prevent locking `catalogsearch_result` and `catalogsearch_query` tables.
+     * Removed transaction wrapper.
+     */
+    public function reindexAll()
+    {
+        try {
+            $this->_getIndexer()->rebuildIndex();
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
